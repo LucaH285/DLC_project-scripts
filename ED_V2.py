20a02683feade1cdaef4d59f64a7d4dae295766f
Luca <lucah1@hotmail.com>
2020-08-21 17:13:19 -0700

    Update ED_V2.py

--- ED_V2.py
+++ ED_V2.py
@@ -8,10 +8,9 @@ Created on Tue Aug  4 17:40:38 2020
 import pandas as pd
 import numpy as np
 import os
-import glob
 
 class Euclidean_Distance():
-    source = os.chdir(r'E:\work\20191206-20200507T194022Z-001\20191206\ED_Files')
+    source = []
     body_part_names = []
     cut_off = 0.95
     def len_bparts(self):
@@ -22,22 +21,31 @@ class Euclidean_Distance():
             return element
         except ValueError:
             pass
-    def file_locations(self):
-        files = []
-        extension = "csv"
-        for file in glob.glob('*.{}'.format(extension)):
-            files.append(file)
-        return files
-    def export_location(self, export, name = '', sep = ',', index = False):
-        ex = export.to_csv(name, sep = sep, index = index)
-        return ex
-    def split_data(self, vector, number_of_body_parts = 4):
+    def split_data(self, vector, number_of_body_parts = 8):
         vector_length = len(vector)
         split = []
         for body_parts in range(number_of_body_parts):
            split1 = vector[body_parts*vector_length//number_of_body_parts:(body_parts+1)*vector_length//number_of_body_parts]
            split.append(split1)
-        return split  
+        return split
+    def file_locations(self):
+        source = self.source
+        if len(source) >= 1:
+            files = [[] for _ in range(len(source))]
+            i = 0
+            while i < len(source):
+                for filename in os.listdir(source[i]):
+                    if filename.endswith(".csv"):
+                        files[i].append(filename)
+                    else:
+                        pass
+                i += 1
+        else:
+            print("Error: no source files found.")
+        return files
+    def export_location(self, export, name = '', sep = ',', index = False):
+        ex = export.to_csv(name, sep = sep, index = index)
+        return ex  
     def error_checking(self, probability_vector, coordinate_vector):
         for probs in range(len(probability_vector)):
             if probability_vector[probs] < (float(self.cut_off)):
@@ -102,44 +110,80 @@ class Euclidean_Distance():
         Split_likelihood = self.split_data(probabilities, self.len_bparts())
         return Split_x, Split_y, Split_likelihood
     def Compute_Euclidean(self):
-        files = self.file_locations()        
-        for files1 in files:
-            Master_lis = []
-            print("File {0} loaded".format(files1))
-            for b_parts in range(0, int(self.len_bparts())):
-                print("processing {0}".format(self.body_part_names[b_parts]))
-                ############
-                #Error Checking
-                ############
-                Error_check_x_cor = self.error_checking(self.dataframe_generator(files1)[2][b_parts], self.dataframe_generator(files1)[0][b_parts])
-                Error_check_y_cor = self.error_checking(self.dataframe_generator(files1)[2][b_parts], self.dataframe_generator(files1)[1][b_parts])
-                print("Check finished")
-                ############
-                #ED computation
-                ############
-                comp_1_x = self.Euclidean_distance(Error_check_x_cor)
-                comp_1_y = self.Euclidean_distance(Error_check_y_cor)
-                
-                comp_2 = self.Euclidean_distance_2(comp_1_x, comp_1_y)
-                print("computation finished")
-                Master_lis.append(comp_2)
-            data_array = np.array([i for i in Master_lis])
-            Master_lis_df = pd.DataFrame([i for i in data_array])
-            Master_lis_df = Master_lis_df.transpose()
-            for b_parts in range(0, self.len_bparts()):
-                Master_lis_df = Master_lis_df.rename(columns={b_parts:"{0}".format(self.body_part_names[b_parts])})
-            self.export_location(Master_lis_df, name = './ED_Files/Complete_{0}'.format(files1), sep = ',', index = False)
-            print("export complete!")
-        return Master_lis
-                
+        files = self.file_locations()
+        if len(self.source) == 1:        
+            for files1, sources in zip(files, range(len(self.source))):
+                os.chdir(str(self.source[sources]))
+                for i_files in files1:
+                    Master_lis = []
+                    print("File {0} loaded".format(i_files))
+                    for b_parts in range(0, int(self.len_bparts())):
+                        print("processing {0}".format(self.body_part_names[b_parts]))
+                        ############
+                        #Error Checking
+                        ############
+                        Error_check_x_cor = self.error_checking(self.dataframe_generator(i_files)[2][b_parts], self.dataframe_generator(i_files)[0][b_parts])
+                        Error_check_y_cor = self.error_checking(self.dataframe_generator(i_files)[2][b_parts], self.dataframe_generator(i_files)[1][b_parts])
+                        print("Check finished")
+                        ############
+                        #ED computation
+                        ############
+                        comp_1_x = self.Euclidean_distance(Error_check_x_cor)
+                        comp_1_y = self.Euclidean_distance(Error_check_y_cor)
+                        
+                        comp_2 = self.Euclidean_distance_2(comp_1_x, comp_1_y)
+                        print("computation finished")
+                        Master_lis.append(comp_2)
+                    ##########
+                    #Export
+                    ##########
+                    data_array = np.array([i for i in Master_lis])
+                    Master_lis_df = pd.DataFrame([i for i in data_array])
+                    Master_lis_df = Master_lis_df.transpose()
+                    for b_parts in range(0, self.len_bparts()):
+                        Master_lis_df = Master_lis_df.rename(columns={b_parts:"{0}".format(self.body_part_names[b_parts])})
+                    self.export_location(Master_lis_df, name = './ED_Files/Complete_{0}'.format(i_files), sep = ',', index = False)
+                    print("export complete!")
+        elif len(self.source) > 1:
+            for files2, sources in zip(files, range(len(self.source))):
+                os.chdir(str(self.source[sources]))
+                for i_files in files2:
+                    Master_lis = []
+                    print("File {0} loaded, from source {1}".format(i_files, sources))
+                    for b_parts in range(0, int(self.len_bparts())):
+                        print("processing {0}".format(self.body_part_names[b_parts]))
+                        ############
+                        #Error Checking
+                        ############
+                        Error_check_x_cor = self.error_checking(self.dataframe_generator(i_files)[2][b_parts], self.dataframe_generator(i_files)[0][b_parts])
+                        Error_check_y_cor = self.error_checking(self.dataframe_generator(i_files)[2][b_parts], self.dataframe_generator(i_files)[1][b_parts])
+                        print("Check finished")
+                        ############
+                        #ED computation
+                        ############
+                        comp_1_x = self.Euclidean_distance(Error_check_x_cor)
+                        comp_1_y = self.Euclidean_distance(Error_check_y_cor)
+                        
+                        comp_2 = self.Euclidean_distance_2(comp_1_x, comp_1_y)
+                        print("computation finished")
+                        Master_lis.append(comp_2)
+                    ##########
+                    #Export
+                    ##########
+                    data_array = np.array([i for i in Master_lis])
+                    Master_lis_df = pd.DataFrame([i for i in data_array])
+                    Master_lis_df = Master_lis_df.transpose()
+                    for b_parts in range(0, self.len_bparts()):
+                        Master_lis_df = Master_lis_df.rename(columns={b_parts:"{0}".format(self.body_part_names[b_parts])})
+                    self.export_location(Master_lis_df, name = './ED_Files/Complete_{0}'.format(i_files), sep = ',', index = False)
+                    print("export complete!")
+        End_statement = "All processing complete, check the export location(s)"
+        return End_statement
 ##############
 #Class calling
 ##############
 ED=Euclidean_Distance()
-ED.source = os.chdir(r'E:\work\20191206-20200507T194022Z-001\20191206')
+ED.source = [r'C:\Users\Desktop\Desktop\work\20191205-20200507T192029Z-001\20191205\Test2']
 ED.cut_off = 0.95
-ED.body_part_names = ["Nose", "Head", "Body", "Tail"]
-ED.Compute_Euclidean()
-    
-    
-
+ED.body_part_names = ["Head", "Neck", "Back", "Tail", "back_right_leg","back_left_leg", "front_right_leg", "front_left_leg"]
+print(ED.Compute_Euclidean())
